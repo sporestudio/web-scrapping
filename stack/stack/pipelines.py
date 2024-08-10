d08d7698e792d8f627f5991a9b76e15c06d1ffb9
sporestudio <spore-wsl@C1-200>
2024-08-11 01:04:38 +0200

    Made MongoDB pipeline and established MongoDB settings

--- stack/stack/pipelines.py
+++ stack/stack/pipelines.py
@@ -3,11 +3,35 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
 
-# useful for handling different item types with a single interface
+from scrapy.utils.project import get_project_settings
+from scrapy.exceptions import DropItem
+from scrapy import log
 from itemadapter import ItemAdapter
 
 
-class StackPipeline:
+settings = get_project_settings()
+
+    
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            valid = False
+            raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+            log.msg("Question added to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
         return item
--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -91,3 +91,12 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+## Database settings
+
+ITEM_PIPELINES = ['stack.pipelines.MongoDBPipeline', ]
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "stackoverflow"
+MONGODB_COLLECTION = "questions"
